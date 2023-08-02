a7cc8647c767a5a956b6f4ebd8af0b79e0d15e61
prachihirekha <prachihirekhan32@gmail.com>
2023-08-02 16:15:22 +0530

    tree leetcode

---
+++ src/main/java/bootcoding/leetcodeque/SymmetricTree.java
@@ -0,0 +1,11 @@
+package bootcoding.leetcodeque;
+
+//public class SymmetricTree {
+//    public static boolean isSymmetric(TreeNode1 root){
+//        if(root==null){
+//            return false;
+//        }
+////        public static boolean isMirror(TreeNode1 )
+////
+////    }
+//}
