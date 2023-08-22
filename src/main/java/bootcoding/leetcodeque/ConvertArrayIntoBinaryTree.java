82108300818868b1b87311108bbba65ab4c1ca60
prachihirekha <prachihirekhan32@gmail.com>
2023-08-22 19:40:44 +0530

    leetcode practice

---
+++ src/main/java/bootcoding/leetcodeque/ConvertArrayIntoBinaryTree.java
@@ -0,0 +1,20 @@
+package bootcoding.leetcodeque;
+
+//public class ConvertArrayIntoBinaryTree {
+//    int [] n;
+//    public TreeNode1 sortedArrayToBST(int[] nums){
+//        n=nums;
+//        return buildBST(0,nums.length-1);
+//    }
+//    public TreeNode1 buildBST(int a,int b){
+//        if(a>b){
+//            return  null;
+//        }
+//        int mid =(a+b)/2;
+//        TreeNode1 cur =new TreeNode1(n[mid]){
+//        public TreeNode1 ConvertedArrayInToBinaryTree (n[mid]){
+//
+//        }
+//    }
+//    }
+//}
