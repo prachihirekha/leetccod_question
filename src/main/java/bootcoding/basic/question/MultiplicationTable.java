7c38cfe637bc2b4ff287bc59733932c7defcb63f
prachihirekha <prachihirekhan32@gmail.com>
2023-10-13 11:44:03 +0530

    practice with easy problem

---
+++ src/main/java/bootcoding/basic/question/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package bootcoding.basic.question;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int k=23;
+        for (int i=1;i<=10;i++){
+            System.out.println(k + "*" +i+ "="+k*i);
+
+        }
+    }
+}
