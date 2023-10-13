6ea5437e97e2f4a129bf7622de41359e4c0a6e61
prachihirekha <prachihirekhan32@gmail.com>
2023-10-13 11:44:27 +0530

    practice with easy problem

---
+++ src/main/java/bootcoding/basic/question/PrintPattern.java
@@ -0,0 +1,10 @@
+package bootcoding.basic.question;
+
+public class PrintPattern {
+    public static void main(String[] args) {
+        System.out.println("  J    a   v     v  a   \n" +
+                          "   J   a a   v   v  a a  \n" +
+                          "J  J  aaaaa   V V  aaaa  \n" +
+                          " JJ  a     a   V  a     a");
+    }
+}
---
+++ src/main/java/bootcoding/basic/question/ProductOfTwoNumber.java
@@ -0,0 +1,11 @@
+package bootcoding.basic.question;
+
+public class ProductOfTwoNumber {
+    public static void main(String[] args) {
+        int a=25;
+        int b =5;
+         int c =a*b;
+        System.out.println(a+"*"+b+"="+c);
+
+    }
+}
