251a242797a76021fbc1b1b23a10023cc13b265d
prachihirekha <prachihirekhan32@gmail.com>
2023-10-13 11:44:41 +0530

    practice with easy problem

---
+++ src/main/java/bootcoding/basic/question/Specified.java
@@ -0,0 +1,16 @@
+package bootcoding.basic.question;
+
+public class Specified {
+    public static void main(String[] args) {
+//        double a =((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+//        double b =4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+//        System.out.println(a);
+//        System.out.println(b);
+        double radius =7.5;
+        double perimeter = 47.12388980384689;
+       double total =Math.PI*radius*radius;
+       double p =Math.PI*radius;
+        System.out.println(p);
+        System.out.println(total);
+    }
+}
