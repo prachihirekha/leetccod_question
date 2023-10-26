e1fcbbff42466e91e61179a4dc67e395d12173a3
prachihirekha <prachihirekhan32@gmail.com>
2023-10-26 11:25:42 +0530

    practice with easy problem

---
+++ src/main/java/bootcoding/basic/question/Fun.java
@@ -0,0 +1,12 @@
+package bootcoding.basic.question;
+
+public class Fun {
+    public static void main(String[] args) {
+        Integer n =10;
+        System.out.println(n);
+        String a ="Suhag";
+        a="prachi";
+        System.out.println(a);
+
+    }
+}
---
+++ src/main/java/bootcoding/basic/question/PerimeterAndArea.java
@@ -0,0 +1,14 @@
+package bootcoding.basic.question;
+
+public class PerimeterAndArea {
+    public static void main(String[] args) {
+        double height =5.5;
+        double width =8.5;
+        double Area =height*width;
+        double  perimeter =2*(height+width);
+        System.out.println(Area);
+        System.out.println(perimeter);
+
+
+    }
+}
