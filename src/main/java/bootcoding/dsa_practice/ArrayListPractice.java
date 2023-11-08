012760b222e50f761009cd0c659e8d9ab1f12425
prachihirekha <prachihirekhan32@gmail.com>
2023-11-08 11:33:40 +0530

    ArrayList practice

---
+++ src/main/java/bootcoding/dsa_practice/ArrayListPractice.java
@@ -0,0 +1,28 @@
+package bootcoding.dsa_practice;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> array= new ArrayList<>();
+        array.add(10);
+        array.add(20);
+        array.add(34);
+        array.add(60);
+        System.out.println(array);
+        // get by index
+        System.out.println(array.get(2));
+        array.add(0,20);
+        System.out.println(array);
+        array.toString();
+        System.out.println(array);
+        // remove
+        array.remove(0);
+        System.out.println(array);
+        System.out.println(array.contains(1));
+        System.out.println(array.contains(60));
+       // is empty
+        System.out.println(array.isEmpty());
+    }
+
+}
