b1f32c7e7d8dc56db82e1330d5dfdabdbae74090
prachihirekha <prachihirekhan32@gmail.com>
2023-11-08 11:33:23 +0530

    ArrayList practice

---
+++ src/main/java/bootcoding/dsa_practice/AddColor.java
@@ -0,0 +1,16 @@
+package bootcoding.dsa_practice;
+
+import java.util.ArrayList;
+
+public class AddColor {
+    public static void main(String[] args) {
+        ArrayList<String> arr=new ArrayList<>();
+        arr.add("Red");
+        arr.add("Yellow");
+        arr.add("Green");
+        arr.add("pink");
+        arr.add("blue");
+        arr.add("brown");
+        System.out.println(arr);
+    }
+}
