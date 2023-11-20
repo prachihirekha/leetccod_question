6d19df0b1bf6954645ed3a13ed53a669f549893e
prachihirekha <prachihirekhan32@gmail.com>
2023-11-20 15:38:50 +0530

    leetcode sqrt practice

---
+++ src/main/java/bootcoding/leetcodeque/leetcode/SquareRoot.java
@@ -0,0 +1,28 @@
+package bootcoding.leetcodeque.leetcode;
+
+public class SquareRoot {
+        public static int mySqrt(int x) {
+            long s =1;
+            long e =x;
+            long ans=0;
+            while(s<=e){
+                long mid =s+(e-s)/2;
+                if(mid*mid==x){
+                    ans=(int)mid;
+                    break;
+                }else if(mid*mid<x){
+                    s=mid+1;
+                    ans =mid;
+                }else{
+                    e=mid-1;
+                }
+            }
+            return (int)ans;
+        }
+
+    public static void main(String[] args) {
+        int n=mySqrt(16);
+        System.out.println(n);
+    }
+    }
+
