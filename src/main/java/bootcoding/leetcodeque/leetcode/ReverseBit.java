a051ffbc543eac88d209343bec0926489c56f870
prachihirekha <prachihirekhan32@gmail.com>
2023-11-24 13:26:41 +0530

    basic question

---
+++ src/main/java/bootcoding/leetcodeque/leetcode/ReverseBit.java
@@ -0,0 +1,18 @@
+//package bootcoding.leetcodeque.leetcode;
+//
+//import java.util.Scanner;
+//import java.util.Stack;
+//
+//public class ReverseBit {
+//    public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter the the number");
+//    }
+//public static int reverse(int n){
+//      int rev =0;
+//        for (int i=0;i<32;i++){
+//            rev=rev<1;
+//        }
+//        return rev;
+//}
+//}
