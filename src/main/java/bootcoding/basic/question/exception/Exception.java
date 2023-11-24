b614a27a7cbbcb75718a206db26a7554f6e5c437
prachihirekha <prachihirekhan32@gmail.com>
2023-11-24 13:26:17 +0530

    basic question

---
+++ src/main/java/bootcoding/basic/question/exception/Exception.java
@@ -0,0 +1,34 @@
+//package bootcoding.basic.question.exception;
+//
+//import java.util.Scanner;
+//
+//class MyException extends Exception{
+//    @Override
+//    public String toString() {
+//        return super.toString()+"i anm to string";
+//    }
+//@Override
+//    public String getMessage(){
+//        return "i am get message";
+//    }
+//}
+//public abstract class Exception {
+//
+//    public static void main(String[] args) {
+//        int a;
+//        Scanner sc =new Scanner(System.in);
+//        a=sc.nextInt();
+//        if (a<88){
+//            try{
+//
+//
+//            }catch (Exception e){
+//                System.out.println(e.getMessage());
+//            }
+//        }
+//
+//    }
+//
+//
+//    public abstract String getMessage();
+//}
