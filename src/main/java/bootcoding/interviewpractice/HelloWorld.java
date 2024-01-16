0c6d69d6ca9e092a238f5e4efff9f9d022e5a5a3
prachihirekha <prachihirekhan32@gmail.com>
2024-01-16 16:30:24 +0530

    program

---
+++ src/main/java/bootcoding/interviewpractice/HelloWorld.java
@@ -0,0 +1,8 @@
+package bootcoding.interviewpractice;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String n ="Hello World";
+        System.out.println(n);
+    }
+}
