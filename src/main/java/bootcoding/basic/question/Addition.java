f1eeda435ccf4522407faf2b21d47c7c33d98b2b
prachihirekha <prachihirekhan32@gmail.com>
2024-03-21 17:13:06 +0530

    Addition

---
+++ src/main/java/bootcoding/basic/question/Addition.java
@@ -0,0 +1,10 @@
+package bootcoding.basic.question;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a =20;
+        int b =20;
+        int c =a+b;
+        System.out.println(c);
+    }
+}
